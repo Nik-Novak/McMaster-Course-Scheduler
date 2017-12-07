1e3d7e01ee66b84ca01b0a273c4d595a945b43d6
Kunal Shah <kunal8@gmail.com>
2017-12-06 22:31:52 -0500

    left menu accordion

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,20 @@ jQuery(window).load(function() {
 		jQuery("#preloader").delay(100).fadeOut("slow");
 		jQuery("#load").delay(100).fadeOut("slow");
 });
+
+// 
+var acc = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+
+// -------------------------------------------------------
