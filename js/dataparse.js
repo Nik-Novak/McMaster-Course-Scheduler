9678b4e1f6d2dc202bdf64fe2b9454d675175c84
Nik-Novak <N.Novak360@gmail.com>
2017-12-07 08:38:35 -0500

    all parsing instances called

--- js/dataparse.js
+++ js/dataparse.js
@@ -4,11 +4,22 @@ $.getJSON("data/databank.json", function(json) {
     console.log(json.timetables[2017][6].courses);//console.log(Object.keys(json.timetables[2017][6].courses).length);
 //    sortResults();
    var courses = convertKeysToArray(json.timetables[2017][6].courses);
+    sortResults(courses, 'department', true);
     sortResults(courses, 'name', true);
+    sortResults(courses, 'code', true);
     console.log(courses);
 });
 
 function sortResults(array, prop, asc) {
+    if(prop=='code')
+        return array.sort(function(a, b) {
+        if (asc) {
+            return (a[prop].split(' ')[1] > b[prop].split(' ')[1]) ? 1 : ((a[prop].split(' ')[1] < b[prop].split(' ')[1]) ? -1 : 0);
+        } else {
+            return (b[prop].split(' ')[1] > a[prop].split(' ')[1]) ? 1 : ((b[prop].split(' ')[1] < a[prop].split(' ')[1]) ? -1 : 0);
+        }
+    });
+    else
     return array.sort(function(a, b) {
         if (asc) {
             return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
