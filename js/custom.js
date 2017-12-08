e411b7f0b47c5fafbe741a3d75f15e2506773101
MatthewShortt <mattshortt4@gmail.com>
2017-12-08 00:24:47 -0500

    calendar css and js changes

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,38 @@
-jQuery(window).load(function() { 
-		jQuery("#preloader").delay(100).fadeOut("slow");
-		jQuery("#load").delay(100).fadeOut("slow");
+jQuery(window).load(function () {
+    jQuery("#preloader").delay(100).fadeOut("slow");
+    jQuery("#load").delay(100).fadeOut("slow");
 });
 
 
 // toggle screen
-$(".screenswitch").click(function(){
+$(".screenswitch").click(function () {
     var screen = $('#coursecontent').css('display');
-    if(screen === "inline-block"){
-        $( "#coursecontent" ).css( "display", "none" );
-        $( "#menu" ).css( "display", "inline-block" );
-//        $("#coursecontent").animate({width:'toggle'},350);
+    if (screen === "inline-block") {
+        $("#coursecontent").css("display", "none");
+        $("#menu").css("display", "inline-block");
+        //        $("#coursecontent").animate({width:'toggle'},350);
+    } else {
+        $("#coursecontent").css("display", "inline-block");
+        $("#menu").css("display", "none");
     }
-    else{
-        $( "#coursecontent" ).css( "display", "inline-block" );
-        $( "#menu" ).css( "display", "none" );
-    }   
 })
 
-$(".remWeekend").click(function(){
+$(".remWeekend").click(function () {
     var tableDisplay = $('.sun').css('display');
-    if(tableDisplay === "table-cell"){
-        $( ".sun" ).css( "display", "none" );
-        $( ".sat" ).css( "display", "none" );
+    if (tableDisplay === "table-cell") {
+        $(".sun").css("display", "none");
+        $(".sat").css("display", "none");
+    } else {
+        $(".sun").css("display", "table-cell");
+        $(".sat").css("display", "table-cell");
     }
-    else{
-        $( ".sun" ).css( "display", "table-cell" );
-        $( ".sat" ).css( "display", "table-cell" );
-    }   
 })
-
-
-
-
-
 
 // accordion collapse
 var acc = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
+    acc[i].onclick = function () {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
@@ -48,4 +41,95 @@ for (var i = 0; i < acc.length; i++) {
             panel.style.display = "block";
         }
     }
+}
+
+
+
+
+
+function dayChange(dayNum) {
+    var dayClass;
+    switch (dayNum) {
+        case 1:
+            dayClass = ".mon";
+            break;
+        case 2:
+            dayClass = ".tues";
+            break;
+        case 3:
+            dayClass = ".wed";
+            break;
+        case 4:
+            dayClass = ".thur";
+            break;
+        case 5:
+            dayClass = ".fri";
+            break;
+        default:
+            dayClass = ".sat";
+    }
+    return dayClass;
+}
+
+
+function startTimeConvert(start) {
+    var startTimeClass;
+    switch (start) {
+        case "8:30":
+            startTimeClass = ".eightThirtyAM";
+            break;
+        case "9:30":
+            startTimeClass = ".nineThirtyAM";
+            break;
+        case "10:30":
+            startTimeClass = ".tenThirtyAM";
+            break;
+        case "11:30":
+            startTimeClass = ".elevenThirtyAM";
+            break;
+        case "12:30":
+            startTimeClass = ".twelveThirtyPM";
+            break;
+        case "13:30":
+            startTimeClass = ".oneThirtyPM";
+            break;
+        case "14:30":
+            startTimeClass = ".twoThirtyPM";
+            break;
+        case "15:30":
+            startTimeClass = ".threeThirtyPM";
+            break;
+        case "16:30":
+            startTimeClass = ".fourThirtyPM";
+            break;
+        case "17:30":
+            startTimeClass = ".fiveThirtyPM";
+            break;
+        case "18:30":
+            startTimeClass = ".sixThirtyPM";
+            break;
+        case "19:00":
+            startTimeClass = ".sevenPM";
+            break;
+        case "20:00":
+            startTimeClass = ".eightPM";
+            break;
+        case "21:00":
+            startTimeClass = ".ninePM";
+            break;
+        default:
+            startTimeClass = ".";
+    }
+    return startTimeClass;
+}
+
+
+
+
+calInput("hello", "hello", "17:30", "hello", 1);
+//input to calendar
+function calInput(courseCode, instructor, startTime, endTime, day) {
+    var dayClass = dayChange(day);
+    var startTimeClass = startTimeConvert(startTime);
+    $(startTimeClass + " " + dayClass).css("background-color", "yellow");
 }
